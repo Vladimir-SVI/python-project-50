5e74610dd364724b2c09061be09ce2a25ffeb970
SVI <svinovoch@yandex.ru>
2023-06-05 13:30:05 +0300

    added YAML format work and tests  file paths changed

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -18,8 +18,8 @@ def test_generate_diff_yml():
     assert diff == result
 
 def test_parse():
-    parse_test = parse('/home/workmain/python-project-50/tests/fixtures/file1.json')
-    file = open('/home/workmain/python-project-50/tests/fixtures/parse_result.py', 'r')
+    parse_test = parse('tests/fixtures/file1.json')
+    file = open('tests/fixtures/parse_result.py', 'r')
     string = file.read()
     res_dict = ast.literal_eval(string)
     assert parse_test == res_dict
